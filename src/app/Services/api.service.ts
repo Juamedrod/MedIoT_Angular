ca750a2ad0d944f8dc5943da97f74949270626fa
JM_Medina <juamedrod@hotmail.es>
2021-11-28 23:49:12 +0100

    Blackboard Start

---
+++ src/app/Services/api.service.ts
@@ -0,0 +1,10 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private httpClient: HttpClient) { }
+}
