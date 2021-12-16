f0b6d2b97ce7e254c0db62593e86fcef675e89b6
JM_Medina <juamedrod@hotmail.es>
2021-12-16 11:16:17 +0100

    Production ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseURL: 'https://mediot.herokuapp.com/api/',
+  baseURL: 'https://mediot.herokuapp.com',
   downloadURL: 'https://mediot.herokuapp.com/'
   /* baseURL: 'http://192.168.1.39:3000/api/',
   downloadURL: 'http://192.168.1.39:3000/' */
