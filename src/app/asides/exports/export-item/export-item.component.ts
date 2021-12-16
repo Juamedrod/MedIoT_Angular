d3590e41c4ecb5d72578e9a741b2f3761d877a8c
JM_Medina <juamedrod@hotmail.es>
2021-12-16 23:46:39 +0100

    Fixed a problem for CSV files downloads, formatting moved to client side instead of back side

--- src/app/asides/exports/export-item/export-item.component.ts
+++ src/app/asides/exports/export-item/export-item.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Device } from 'src/app/Interfaces/device.interface';
 import { msgType } from 'src/app/Interfaces/message.interface';
+import { HistoricService } from 'src/app/Services/historic.service';
 import { LogService } from 'src/app/Services/log.service';
 import { MessagesSystemService } from 'src/app/Services/messages-system.service';
+import { environment } from 'src/environments/environment';
+import { AngularCsv } from 'angular-csv-ext/dist/Angular-csv';
 
 @Component({
   selector: 'export-item',
@@ -15,10 +18,10 @@ export class ExportItemComponent implements OnInit {
   maxLimitOfData: string;
   @Input() device!: Device;
 
-  constructor(private logService: LogService, private messageService: MessagesSystemService) {
+  constructor(private historicService: HistoricService, private messageService: MessagesSystemService) {
     this.selectExport = '';
     this.maxLimitOfData = '0';
-    this.downloadURL = 'http://192.168.1.39:3000/';
+    this.downloadURL = environment.baseURL;
   }
 
   ngOnInit(): void {
@@ -26,18 +29,68 @@ export class ExportItemComponent implements OnInit {
 
   async downloadSCV(dId: string) {
     try {
-      const response = await this.logService.downloadCSV(dId, this.maxLimitOfData, this.selectExport);
-      window.location.href = `${this.downloadURL}${response.url}`;
-      if (response.url) {
-        this.messageService.newMessage({ message: 'File download ready!', messageType: msgType.success });
-        return response.url;
+      const response = await this.historicService.getData(dId, this.maxLimitOfData);
+      if (response.length > 0) {
+        const data = [];
+        switch (this.selectExport) {
+          case 'variables':
+            let varKeys = Object.keys(response[0].variables);
+            for (const item of response) {
+              data.push({
+                dId: item.dId,
+                iat: item.iat,
+                ...item.variables
+              });
+            }
+            var options = {
+              fieldSeparator: ',',
+              quoteStrings: '"',
+              decimalseparator: '.',
+              showLabels: true,
+              showTitle: true,
+              title: 'Your title',
+              useBom: true,
+              noDownload: false,
+              headers: ["dId", "iat", ...varKeys],
+              useHeader: true,
+              nullToEmptyString: true,
+            };
+            new AngularCsv(data, `${dId}.csv`, options);
+            break;
+
+          default:
+            for (const item of response) {
+              let obj = {
+                dId: item.dId,
+                iat: item.iat,
+                ...item.variables[this.selectExport]
+              }
+              obj[this.selectExport] = item.variables[this.selectExport];
+              data.push(obj);
+            }
+            var options = {
+              fieldSeparator: ',',
+              quoteStrings: '"',
+              decimalseparator: '.',
+              showLabels: true,
+              showTitle: true,
+              title: 'Your title',
+              useBom: true,
+              noDownload: false,
+              headers: ["dId", "iat", this.selectExport],
+              useHeader: true,
+              nullToEmptyString: true,
+            };
+            new AngularCsv(data, `${dId}.csv`, options);
+            break;
+        }
+
       }
-      this.messageService.newMessage({ message: 'An error has ocurred!', messageType: msgType.error });
+      this.messageService.newMessage({ message: 'File download ready!', messageType: msgType.success });
       return '';
     } catch (error) {
       this.messageService.newMessage({ message: 'An error has ocurred!', messageType: msgType.error });
       return '';
     }
   }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseURL: 'https://mediot.herokuapp.com',
+  baseURL: 'http://192.168.1.39:3000',
 
 };
 
